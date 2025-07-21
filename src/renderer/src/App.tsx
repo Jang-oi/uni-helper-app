24535b0620bcd1e5d028111a4942238b881f143b
jeongho <jeongho@unipost.co.kr>
2025-07-21 14:29:03 +0900

    Initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "./components/ui/button";
+
+function App(): React.JSX.Element {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button variant="default" size="lg">
+        ShadCN Button
+      </Button>
+    </div>
+  )
+}
+
+export default App
