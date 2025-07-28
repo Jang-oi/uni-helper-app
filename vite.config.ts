ac155d83a22c8afdac75ce835d743bf9ca94b85f
Jang-oi <jjh53241@gmail.com>
2025-07-28 15:14:17 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src/renderer/src')
-      }
+export default defineConfig(async () => {
+  const tailwindcss = (await import('@tailwindcss/vite')).default
+  return {
+    main: {
+      plugins: [externalizeDepsPlugin()]
     },
-    plugins: [react(), tailwindcss()]
+    preload: {
+      plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src/renderer/src')
+        }
+      },
+      plugins: [react(), tailwindcss()]
+    }
   }
 })
